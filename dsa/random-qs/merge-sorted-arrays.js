4c97e2cdae83343969bc8ae8113b5eaa6cad1e91
Akshay <119416860+axai-kaizoku@users.noreply.github.com>
2025-08-07 04:04:09 +0000

    add moore's voting algorithm

---
+++ dsa/random-qs/merge-sorted-arrays.js
@@ -0,0 +1,25 @@
+const arr1 = [1, 2, 3, 0, 0, 0]
+const arr2 = [2, 5, 6]
+const a1 = 3
+const a2 = 3
+
+function getSortedArray(nums1, m, nums2, n) {
+  let i = m - 1,
+    j = n - 1,
+    k = m + n - 1
+
+  while (j >= 0) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i]
+      i--
+    } else {
+      nums1[k] = nums2[j]
+      j--
+    }
+    k--
+  }
+}
+
+getSortedArray(arr1, a1, arr2, a2)
+
+console.log(arr1)
